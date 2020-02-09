f3021c2ebdbad3953714e4046165de2b6834ac87
eleicha <laura.mons@web.de>
2020-02-09 13:58:54 +0100

    drawings and changes

--- createGraphs.py
+++ createGraphs.py
@@ -55,35 +55,35 @@ def draw_scatter_message_length(x1, x2, x3, x4, y, name, title, x_label, y_label
 
 	fig1, ax1 = plt.subplots()
 
-	ax1.scatter(x_protobuf, y_protobuf, c='g')
-	ax1.scatter(x_capnp, y_capnp, color='b')
-	ax1.scatter(x_avro, y_avro, color='r')
-	ax1.scatter(x_xml, y_xml, color='k')
+	proto = ax1.scatter(x_protobuf, y_protobuf, c='g')
+	capnp = ax1.scatter(x_capnp, y_capnp, color='b')
+	avro = ax1.scatter(x_avro, y_avro, color='r')
+	xml = ax1.scatter(x_xml, y_xml, color='k')
 
 	#plt.legend((y_protobuf, y_capnp, y_avro, y_xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'), scatterpoints=1)
 
-	ax1.ylabel(y_label)
-	ax1.xlabel(x_label)
-	ax1.legend(*scatter.legend_elements())
+	ax1.set_ylabel(y_label)
+	ax1.set_xlabel(x_label)
+	ax1.legend((proto, capnp, avro, xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'))
 	title = title + ' Measured on ' + str(machine).capitalize()
 	if int(print_to_file) == 1:
 		title = title + ' (Data Written to Disk)'
-	ax1.title(str(title))
+	ax1.set_title(str(title))
 	#plt.xticklabels(['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
 
 	fig1.savefig('eval/scatter_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
 
-def draw_scatter(x, y, name, title, x_label, y_label):
+def draw_scatter(x, scale_x, y, scale_y, name, title, x_label, y_label):
 	
-	x_protobuf = np.asarray(x[0])
-	x_capnp = np.asarray(x[1])
-	x_avro = np.asarray(x[2])
-	x_xml = np.asarray(x[3])
+	x_protobuf = np.asarray(x[0])-scale_x[0]
+	x_capnp = np.asarray(x[1])-scale_x[1]
+	x_avro = np.asarray(x[2])-scale_x[1]
+	x_xml = np.asarray(x[3])-scale_x[1]
 
-	y_protobuf = np.asarray(y[0])
-	y_capnp = np.asarray(y[1])
-	y_avro = np.asarray(y[2])
-	y_xml = np.asarray(y[3])
+	y_protobuf = np.asarray(y[0])-scale_y[0]
+	y_capnp = np.asarray(y[1])-scale_y[1]
+	y_avro = np.asarray(y[2])-scale_y[2]
+	y_xml = np.asarray(y[3])-scale_y[3]
 
 	#print('len prot ', len(x_protobuf), 'len y ', len(y_protobuf))
 
@@ -94,25 +94,25 @@ def draw_scatter(x, y, name, title, x_label, y_label):
 
 	fig2, ax2 = plt.subplots()
 
-	ax2.scatter(x_protobuf, y_protobuf, c='g')
-	ax2.scatter(x_capnp, y_capnp, color='b')
-	ax2.scatter(x_avro, y_avro, color='r')
-	ax2.scatter(x_xml, y_xml, color='k')
+	proto = ax2.scatter(x_protobuf, y_protobuf, c='g')
+	capnp = ax2.scatter(x_capnp, y_capnp, color='b')
+	avro = ax2.scatter(x_avro, y_avro, color='r')
+	xml = ax2.scatter(x_xml, y_xml, color='k')
 
 	#plt.legend((y_protobuf, y_capnp, y_avro, y_xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'), scatterpoints=1)
 
-	ax2.ylabel(y_label)
-	ax2.xlabel(x_label)
-	ax2.legend(*scatter.legend_elements())
+	ax2.set_ylabel(y_label)
+	ax2.set_xlabel(x_label)
+	ax2.legend((proto, capnp, avro, xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'))
 	title = title + ' Measured on ' + str(machine).capitalize()
 	if int(print_to_file) == 1:
 		title = title + ' (Data Written to Disk)'
-	ax2.title(str(title))
+	ax2.set_title(str(title))
 	#plt.xticklabels(['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
 
 	fig2.savefig('eval/scatter_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
 
-def draw_scatter_disk(x, start_value_x, bytes y, start_value_y, name, title, x_label, y_label):
+def draw_scatter_disk(x, start_value_x, y, start_value_y, name, title, x_label, y_label):
 	
 	x_protobuf = np.asarray(x[0])-start_value_x[0]
 	x_capnp = np.asarray(x[1])-start_value_x[1]
@@ -124,10 +124,10 @@ def draw_scatter_disk(x, start_value_x, bytes y, start_value_y, name, title, x_l
 	y_avro = np.asarray(y[2])-start_value_y[2]
 	y_xml = np.asarray(y[3])-start_value_y[3]
 
-	print('len prot ', len(x_protobuf), 'len y ', len(y_protobuf))
-	print('len prot ', len(x_xml), 'len y ', len(y_xml))
-	print(y_xml)
-	print(x_xml)
+	#print('len prot ', len(x_protobuf), 'len y ', len(y_protobuf))
+	#print('len prot ', len(x_xml), 'len y ', len(y_xml))
+	#print(y_xml)
+	#print(x_xml)
 
 	machine = name.split('_')[0]
 	number_of_people = name.split('_')[2]
@@ -136,30 +136,31 @@ def draw_scatter_disk(x, start_value_x, bytes y, start_value_y, name, title, x_l
 
 	fig3, ax3 = plt.subplots()
 
-	ax3.scatter(x_protobuf, y_protobuf, c='g')
-	ax3.scatter(x_capnp, y_capnp, color='b')
-	ax3.scatter(x_avro, y_avro, color='r')
-	ax3.scatter(x_xml, y_xml, color='k')
+	proto = ax3.scatter(x_protobuf, y_protobuf, c='g')
+	capnp = ax3.scatter(x_capnp, y_capnp, color='b')
+	avro = ax3.scatter(x_avro, y_avro, color='r')
+	xml = ax3.scatter(x_xml, y_xml, color='k')
 
 	#plt.legend((y_protobuf, y_capnp, y_avro, y_xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'), scatterpoints=1)
 
-	ax3.ylabel(y_label)
-	ax3.xlabel(x_label)
-	ax3.legend(*scatter.legend_elements())
+	ax3.set_ylabel(y_label)
+	ax3.set_xlabel(x_label)
+	ax3.legend((proto, capnp, avro, xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'))
 	title = title + ' Measured on ' + str(machine).capitalize()
 	if int(print_to_file) == 1:
-		title = title + ' (Data Written to Disk)'
-	ax3.title(str(title))
+		filetitle = title +'_disk'
+		#title = title + ' (Data Written to Disk)'
+	ax3.set_title(str(title))
 	#plt.xticklabels(['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
 
-	fig3.savefig('eval/scatter_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
+	fig3.savefig('eval/scatter_'+str(filetitle)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file)+'.png', bbox_inches='tight')
 
-draw_scatter_mul_y(x, y1, y2, y3, y4, name, title, x_label, y_label)
+def draw_scatter_mul_y(x,discount, y1, y2, y3, y4, name, title, x_label, y_label):
 	
-	x_protobuf = np.asarray(x[0])
-	x_capnp = np.asarray(x[1])
-	x_avro = np.asarray(x[2])
-	x_xml = np.asarray(x[3])
+	x_protobuf = np.asarray(x[0])-discount[0]
+	x_capnp = np.asarray(x[1])-discount[1]
+	x_avro = np.asarray(x[2])-discount[2]
+	x_xml = np.asarray(x[3])-discount[3]
 
 	y_protobuf = np.asarray(y1)
 	y_capnp = np.asarray(y2)
@@ -173,20 +174,20 @@ draw_scatter_mul_y(x, y1, y2, y3, y4, name, title, x_label, y_label)
 
 	fig4, ax4 = plt.subplots()
 
-	ax4.scatter(x_protobuf, y_protobuf, c='g')
-	ax4.scatter(x_capnp, y_capnp, color='b')
-	ax4.scatter(x_avro, y_avro, color='r')
-	ax4.scatter(x_xml, y_xml, color='k')
+	proto = ax4.scatter(x_protobuf, y_protobuf, c='g')
+	capnp = ax4.scatter(x_capnp, y_capnp, color='b')
+	avro = ax4.scatter(x_avro, y_avro, color='r')
+	xml = ax4.scatter(x_xml, y_xml, color='k')
 
 	#plt.legend((y_protobuf, y_capnp, y_avro, y_xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'), scatterpoints=1)
 
-	ax4.ylabel(y_label)
-	ax4.xlabel(x_label)
-	ax4.legend(*scatter.legend_elements())
+	ax4.set_ylabel(y_label)
+	ax4.set_xlabel(x_label)
+	ax4.legend((proto, capnp, avro, xml), ('Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'))
 	title = title + ' Measured on ' + str(machine).capitalize()
-	if int(print_to_file) == 1:
-		title = title + ' (Data Written to Disk)'
-	ax4.title(str(title))
+	#if int(print_to_file) == 1:
+		#title = title + ' (Data Written to Disk)'
+	ax4.set_title(str(title))
 	#plt.xticklabels(['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
 
 	fig4.savefig('eval/scatter_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
@@ -224,24 +225,27 @@ def draw_line_chart(x, y, name, title, x_label, y_label):
 	#ax.set_xticklabels(['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
 	fig.savefig('eval/line_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
 '''
-def draw_boxplot(values, start_value_bytes_rec, name, title):
+def draw_boxplot_xml(values, start_value_bytes_rec, name, title):
 
 	#print(values)
 	data_to_plot = []
 	val = []
-	for i in range(0, len(values):
+	for i in range(0, len(values)):
 		val.append(values[i])
+		print(values[i])
 		if (i + 1)%10 == 0 :
+			print(val)
 			data_to_plot.append(val)
 			val = []
-	
+	'''
 	protobuf = np.asarray(values[0])-start_value_bytes_rec[0]
 	capnp = np.asarray(values[1])-start_value_bytes_rec[0]
 	avro = np.asarray(values[2])-start_value_bytes_rec[0]
 	xml = np.asarray(values[3])-start_value_bytes_rec[0]
 
 	data_to_plot = [protobuf, capnp, avro, xml]
-
+	'''
+	print(data_to_plot)
 	machine = name.split('_')[0]
 	number_of_people = name.split('_')[2]
 	number_of_messages = name.split('_')[3]
@@ -251,12 +255,13 @@ def draw_boxplot(values, start_value_bytes_rec, name, title):
 
 	#ax = fig.add_subplot(111)
 	ax5.boxplot(data_to_plot)
-	ax5.ylabel(title)
+	ax5.set_ylabel(title)
 	title = title + ' Measured on ' + str(machine).capitalize()
 	if print_to_file == 1:
 		title = title + ' (data written to disk)'
-	ax5.title(str(title))
-	ax5.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],['224', '410', '596', '782', '968','1154','1340','1526','1712','1898','2084','2270','2456','2642','2828','3014','3200','3386','3572','3758','3944','4130','4316','4502','4688'])
+	ax5.set_title(str(title))
+	ax5.set_xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
+	ax5.set_xticklabels(['224', '410', '596', '782', '968','1154','1340','1526','1712','1898','2084','2270','2456','2642','2828','3014','3200','3386','3572','3758','3944','4130','4316','4502','4688'])
 	fig5.savefig('eval/box_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
 
 
@@ -281,12 +286,13 @@ def draw_boxplot(values, start_value_bytes_rec, name, title):
 
 	#ax = fig.add_subplot(111)
 	ax6.boxplot(data_to_plot)
-	ax6.ylabel(title)
+	ax6.set_ylabel(title)
 	title = title + ' Measured on ' + str(machine).capitalize()
-	if print_to_file == 1:
+	if int(print_to_file) == 1:
 		title = title + ' (data written to disk)'
-	ax6.title(str(title))
-	ax6.xticks([1,2,3,4],['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
+	ax6.set_title(str(title))
+	ax6.set_xticks([1,2,3,4])
+	ax6.set_xticklabels(['Protobuf', 'Cap\'n Proto', 'Apache Avro', 'XML'])
 	fig6.savefig('eval/box_'+str(title)+'_'+str(machine)+'_'+str(number_of_people)+'_'+str(number_of_messages)+'_'+str(print_to_file), bbox_inches='tight')
 
 
@@ -322,7 +328,7 @@ def main():
 
 	for path in paths:
 		identify_format = path.split('/')[1].split('_')[1]
-		print(identify_format)
+		#print(identify_format)
 
 		with open(str(path), 'r') as f:
 			Lines = [line.rstrip('\n') for line in f]
@@ -423,7 +429,7 @@ def main():
 				for value in line.split(':')[1].lstrip('[').rstrip(']').split(','):
 					if len(value) != 0:
 						values.append(float(value))
-						print(value)
+						#print(value)
 				bytes_sent.append(values)
 			elif line.split(':')[0] == 'packets_sent':
 				values = []
@@ -469,20 +475,20 @@ def main():
 		draw_scatter_message_length(message_length_proto, message_length_capnp, message_length_avro, message_length_xml, memory, paths[0].split('/')[1], 'Memory Utilization', 'Message Size', 'Memory Utilization in %')
 
 		if int(print_to_file) == 1:
-			draw_scatter_disk(write_time, [0,0,0,0], write_bytes, start_value_disk, paths[0].split('/')[1], 'Bytes Written', 'Time to Write to Disk in ms', 'Data Written in Bytes')
-			draw_scatter_disk(write_bytes, start_value_disk, bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Data Received vs. Data Written', 'Data Received vs. Data Written', 'Data Received in Bytes')
-			draw_scatter(write_time, [0,0,0,0], bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Time to Write Bytes', 'Time to Write Bytes in ms', 'Bytes Received')
-			draw_scatter_mul_y(write_time, message_length_proto, message_length_capnp, message_length_avro, message_length_xml, paths[0].split('/')[1], 'Time to Write Message Length', 'Time to Write Message in ms', 'Message Length in Bytes')
+			draw_scatter_disk(write_time, [write_time[0][0],write_time[1][0],write_time[2][0],write_time[3][0]], write_bytes, start_value_disk, paths[0].split('/')[1], 'Bytes Written', 'Time to Write to Disk in ms', 'Data Written in Bytes')
+			draw_scatter_disk(write_bytes, start_value_disk, bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Data Received vs. Data Writtento Disk', 'Data Written to Disk in Bytes', 'Data Received in Bytes')
+			draw_scatter_disk(write_time, [write_time[0][0],write_time[1][0],write_time[2][0],write_time[3][0]], bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Time to Write Received Bytes', 'Time to Write Bytes in ms', 'Bytes Received')
+			draw_scatter_mul_y(write_time, [write_time[0][0],write_time[1][0],write_time[2][0],write_time[3][0]], message_length_proto, message_length_capnp, message_length_avro, message_length_xml, paths[0].split('/')[1], 'Time to Write Message Length', 'Time to Write Message in ms', 'Message Length in Bytes')
 
 			#draw_scatter(read_time, read_bytes, paths[0].split('/')[1], 'Bytes Read', 'Time to Read data in ms', 'Data Read in Bytes')
 			#draw_scatter(read_time, bytes_recv, paths[0].split('/')[1], 'Bytes Read', 'Time to Write to disk in ms', 'Data Received in Bytes')
 
 		draw_scatter(read_time, [0,0,0,0], bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Time to Read Bytes', 'Time to Read Bytes in ms', 'Bytes Received')
-		draw_scatter_mul_y(read_time, message_length_proto, message_length_capnp, message_length_avro, message_length_xml, paths[0].split('/')[1], 'Time to Read Message Length', 'Time to Read Message in ms', 'Message Length in Bytes')
+		draw_scatter_mul_y(read_time, [0,0,0,0], message_length_proto, message_length_capnp, message_length_avro, message_length_xml, paths[0].split('/')[1], 'Time to Read Message Length', 'Time to Read Message in ms', 'Message Length in Bytes')
 
 
 		draw_boxplot_xml(message_length_xml, start_value_bytes_rec, paths[0].split('/')[1], 'Deviations in Message Sizes')
-		draw_boxplot(bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Bytes Received')
+		#draw_boxplot(bytes_recv, start_value_bytes_rec, paths[0].split('/')[1], 'Bytes Received')
 		draw_boxplot(times, [0,0,0,0], paths[0].split('/')[1], 'Processing Time')
 
 	elif machine == 'client':
---
+++ test_xml_sequence_1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+python3 client.py 1 3 1 10 10 wlanPi
+
