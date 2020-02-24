4dd01a42c733ec14a3e399ae6afc52c5643f3034
eleicha <laura.mons@web.de>
2020-02-24 18:16:15 +0100

    changed to zmq

---
+++ clientZMQ.py
@@ -0,0 +1,268 @@
+import io
+import json
+from avro.datafile import DataFileWriter
+import avro.schema
+from avro.io import DatumWriter
+import avro.ipc
+import struct
+import addressBook_pb2
+import capnp
+import addressbook_capnp
+import psutil
+import os
+import sys
+import time
+import zmq
+
+def send_proto_message(connection, numberOfPeople, i):
+
+    address_book = addressBook_pb2.AddressBook()
+    
+    for x in range(1, int(numberOfPeople)+1):
+
+        person = address_book.people.add()
+        person.id = x
+        person.name = "Rosa Luxemburg"
+        person.email = "rosa.luxemburg@web.de"
+        phone = person.phones.add()
+        phone.number = "0178525048"
+
+    message = address_book.SerializeToString()
+
+    connection.send(message)
+    print("Length of send Proto data: " + str(len(message)))
+    print("Type of Proto data send: " + str(type(message)))
+    print("Iter: " + str(i))
+
+    return(len(message))
+
+
+def send_capnp_message(connection, numberOfPeople, i):
+    
+
+    addresses = addressbook_capnp.AddressBook.new_message()
+    people = addresses.init("people", int(numberOfPeople))
+
+    for x in range(1, int(numberOfPeople)+1):
+        people[x].name = "Rosa Luxemburg"
+        people[x].id = int(i)
+        people[x].email = "rosa.luxemburg@web.de"
+
+        rosaPhone = people[x].init("phones", 1)[0]
+
+        rosaPhone.number = "01785250483"
+
+    message = addresses.to_bytes_packed()
+
+    connection.send(message)
+    print("Length of send Capnp data: " + str(len(message)))
+    print("Type of Capnp data send: " + str(type(message)))
+    print("Iter: " + str(i))
+
+    return(len(message))
+
+
+def send_avro_message(connection, numberOfPeople, i):
+    
+    message = {'id': i, 'name': 'Rosa Luxemburg', 'email': 'rosa.luxemburg@web.de', 'PhoneNumber': '01785250483'}
+    buf = io.BytesIO()
+    schema = avro.schema.Parse(open("schema/addressbook.avsc", "r").read())
+    writer = avro.datafile.DataFileWriter(buf, avro.io.DatumWriter(), schema)
+    for x in range(1, int(numberOfPeople)+1):
+        writer.append(message)
+    writer.flush()
+    buf.seek(0)
+    data = buf.read()
+
+    connection.send(message)
+    print("Length of Avro data: " + str(len(data)))
+    print("Type of Avro data send: " + str(type(data)))
+    print("Iter: " + str(i))
+
+    return(len(data))
+
+def send_XML_message(connection, numberOfPeople, i):
+
+    messages = '<xml version="1.0" encoding="UTF-8"> \n'
+
+    for x in range(1, int(numberOfPeople)+1):
+        message = """<AddressBook><Person><id>"""+str(x)+"""<id/><personName>Rosa Luxemburg<personName/><email>rosa.luxemburg@web.de<email/><PhoneNumber><number>01785250483<number/><PhoneNumber/><Person/><AddressBook/>"""
+        messages += message + """ \n"""
+
+    #xs = xmlschema.XMLSchema('schema/addressbook.xsd')
+    #print(xs.types)
+    #correct = xs.is_valid('schema/addressbook.xml')
+
+    connection.send(message)
+    print("Length of XML data: " + str(len(messages.encode())))
+    print("Type of XML data send: " + str(type(messages.encode())))
+    print("Iter: " + str(i))
+
+    return(len(messages))
+
+def main():
+
+    #0 for protobuf, 1 for cap'n proto, 2 for Apache Avro, and 3 for XML
+    printToFile = sys.argv[1]
+    messageType = sys.argv[2]
+    numberOfPeopleOrigin = sys.argv[3]
+    numberOfMessages = sys.argv[4]
+    numberOfExperiments = sys.argv[5]
+    machinesUsed = sys.argv[6]
+
+    p = psutil.Process(os.getpid())
+    cpu_util = []
+    cpu_util_user = []
+    cpu_util_system = []
+    cpu_util_idle = []
+    memory = []
+    net_io_counters1 = []
+    net_io_counters2 = []
+    start_value_bytes_sent = psutil.net_io_counters().bytes_sent
+    times = []
+    message_length = []
+    context = zmq.Context()
+            
+    for numberOfPeople in range(1,int(numberOfPeopleOrigin)+1):
+
+        #psutil.cpu_times_percent(None, False)
+        socket = context.socket(zmq.REQ)
+        #connection.connect(('172.16.150.67', 12345))
+        socket.connect("tcp://localhost:5555")
+        #connection.connect(('192.168.2.105', 12345))
+        
+        if int(messageType) == 0:
+            for i in range(0,int(numberOfExperiments)):
+
+                psutil.cpu_times_percent(None,False)
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                start = time.perf_counter()
+
+                for x in range(0,int(numberOfMessages)):
+                    mem = send_proto_message(socket, numberOfPeople, i)
+
+                    message_length.append(mem)
+                    times.append(time.perf_counter() - start)
+                    memory.append(psutil.virtual_memory().percent)
+                    cpu_util.append(psutil.cpu_percent(None, False))
+                    net_io_counters1.append(psutil.net_io_counters().bytes_sent)
+                    cpu_util_user.append(psutil.cpu_times_percent(None, False).user)
+                    cpu_util_system.append(psutil.cpu_times_percent(None, False).system)
+                    cpu_util_idle.append(psutil.cpu_times_percent(None, False).idle)
+
+                    answer = socket.recv()
+                    print("Received reply %s: %s" % (i, answer))
+
+            with open('results/client_'+str(messageType)+'_'+str(numberOfPeople)+'_'+str(numberOfMessages)+'_'+str(printToFile)+'_'+str(machinesUsed)+'.txt', 'w') as f:
+                f.write('CPU:'+ str(cpu_util)+'\n')
+                f.write('CPU_USER:'+str(cpu_util_user)+'\n')
+                f.write('CPU_SYSTEM:'+str(cpu_util_system)+'\n')
+                f.write('CPU_IDLE:'+str(cpu_util_idle)+'\n')
+                f.write('MEMORY:'+ str(memory)+'\n')
+                f.write('TIME:'+str(times)+'\n')
+                f.write('message_length:'+str(message_length)+'\n')
+                f.write('bytes_sent:'+str(net_io_counters1)+'\n')
+                f.write('start_value_bytes_sent:'+str(start_value_bytes_sent)+'\n')
+            print('finished')
+
+        elif int(messageType) == 1 :
+            for i in range(0,int(numberOfExperiments)):
+
+                psutil.cpu_times_percent(None,False)
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                start = time.perf_counter()
+
+                for x in range(0,int(numberOfMessages)):
+                    mem = send_capnp_message(socket, numberOfPeople,i)
+                    message_length.append(mem)
+                    times.append(time.perf_counter() - start)
+                    memory.append(psutil.virtual_memory().percent)
+                    cpu_util.append(psutil.cpu_percent(None, False))
+                    net_io_counters1.append(psutil.net_io_counters().bytes_sent)
+                    cpu_util_user.append(psutil.cpu_times_percent(None, False).user)
+                    cpu_util_system.append(psutil.cpu_times_percent(None, False).system)
+                    cpu_util_idle.append(psutil.cpu_times_percent(None, False).idle)
+
+                    answer = socket.recv()
+                    print("Received reply %s: %s" % (i, answer))
+
+            with open('results/client_'+str(messageType)+'_'+str(numberOfPeople)+'_'+str(numberOfMessages)+'_'+str(printToFile)+'_'+str(machinesUsed)+'.txt', 'w') as f:
+                f.write('CPU:'+ str(cpu_util)+'\n')
+                f.write('CPU_USER:'+str(cpu_util_user)+'\n')
+                f.write('CPU_SYSTEM:'+str(cpu_util_system)+'\n')
+                f.write('CPU_IDLE:'+str(cpu_util_idle)+'\n')
+                f.write('MEMORY:'+ str(memory)+'\n')
+                f.write('TIME:'+str(times)+'\n')
+                f.write('message_length:'+str(message_length)+'\n')
+                f.write('bytes_sent:'+str(net_io_counters1)+'\n')
+                f.write('start_value_bytes_sent:'+str(start_value_bytes_sent)+'\n')
+            print('finished')
+        elif int(messageType) == 2 :
+            for i in range(0,int(numberOfExperiments)):
+
+                psutil.cpu_times_percent(None,False)
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                start = time.perf_counter()
+                for x in range(0,int(numberOfMessages)):
+                    mem = send_avro_message(socket, numberOfPeople,i)
+                    message_length.append(mem)
+                    times.append(time.perf_counter() - start)
+                    memory.append(psutil.virtual_memory().percent)
+                    cpu_util.append(psutil.cpu_percent(None, False))
+                    net_io_counters1.append(psutil.net_io_counters().bytes_sent)
+                    cpu_util_user.append(psutil.cpu_times_percent(None, False).user)
+                    cpu_util_system.append(psutil.cpu_times_percent(None, False).system)
+                    cpu_util_idle.append(psutil.cpu_times_percent(None, False).idle)
+
+                    answer = socket.recv()
+                    print("Received reply %s: %s" % (i, answer))
+
+            with open('results/client_'+str(messageType)+'_'+str(numberOfPeople)+'_'+str(numberOfMessages)+'_'+str(printToFile)+'_'+str(machinesUsed)+'.txt', 'w') as f:
+                f.write('CPU:'+ str(cpu_util)+'\n')
+                f.write('CPU_USER:'+str(cpu_util_user)+'\n')
+                f.write('CPU_SYSTEM:'+str(cpu_util_system)+'\n')
+                f.write('CPU_IDLE:'+str(cpu_util_idle)+'\n')
+                f.write('MEMORY:'+ str(memory)+'\n')
+                f.write('TIME:'+str(times)+'\n')
+                f.write('message_length:'+str(message_length)+'\n')
+                f.write('bytes_sent:'+str(net_io_counters1)+'\n')
+                f.write('start_value_bytes_sent:'+str(start_value_bytes_sent)+'\n')
+            print('finished')
+        elif int(messageType) == 3 :
+            for i in range(0,int(numberOfExperiments)):
+
+                psutil.cpu_times_percent(None,False)
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                start = time.perf_counter()
+                for x in range(0,int(numberOfMessages)):
+                    mem=send_XML_message(socket, numberOfPeople,i)
+                    message_length.append(mem)
+                    times.append(time.perf_counter() - start)
+                    memory.append(psutil.virtual_memory().percent)
+                    cpu_util.append(psutil.cpu_percent(None, False))
+                    net_io_counters1.append(psutil.net_io_counters().bytes_sent)
+                    cpu_util_user.append(psutil.cpu_times_percent(None, False).user)
+                    cpu_util_system.append(psutil.cpu_times_percent(None, False).system)
+                    cpu_util_idle.append(psutil.cpu_times_percent(None, False).idle)
+
+                    answer = socket.recv()
+                    print("Received reply %s: %s" % (i, answer))
+
+            with open('results/client_'+str(messageType)+'_'+str(numberOfPeople)+'_'+str(numberOfMessages)+'_'+str(printToFile)+'_'+str(machinesUsed)+'.txt', 'w') as f:
+                f.write('CPU:'+ str(cpu_util)+'\n')
+                f.write('CPU_USER:'+str(cpu_util_user)+'\n')
+                f.write('CPU_SYSTEM:'+str(cpu_util_system)+'\n')
+                f.write('CPU_IDLE:'+str(cpu_util_idle)+'\n')
+                f.write('MEMORY:'+ str(memory)+'\n')
+                f.write('TIME:'+str(times)+'\n')
+                f.write('message_length:'+str(message_length)+'\n')
+                f.write('bytes_sent:'+str(net_io_counters1)+'\n')
+                f.write('start_value_bytes_sent:'+str(start_value_bytes_sent)+'\n')
+            print('finished')
+
+if __name__ == '__main__':
+    main()
---
+++ serverZMQ.py
@@ -0,0 +1,249 @@
+import avro.datafile
+import avro.io
+import io
+import time
+import numpy as np
+from avro.datafile import DataFileWriter
+import avro.schema
+from avro.io import DatumWriter
+import struct
+import capnp
+import addressbook_capnp
+import addressBook_pb2
+import psutil
+import sys
+import os
+import zmq
+
+def handle_proto_client(connection):
+
+    message = connection.recv()
+
+    addresses = addressBook_pb2.AddressBook()
+    addresses.ParseFromString(message)
+
+    print (addresses)
+
+    return(len(message))
+
+def handle_proto_client_print_to_file(connection):
+
+    message = connection.recv()
+
+    #addresses = addressBook_pb2.AddressBook()
+    #addresses.ParseFromString(message)
+
+    f = open("schema/addressbook_proto.bin", "w+b")
+    f.write(message)
+    
+    os.fsync(f)
+    f.close()
+    return(len(message))
+
+def handle_capnp_client(connection):
+
+    message = connection.recv()
+
+    addresses = addressbook_capnp.AddressBook.new_message()
+    addresses = addressbook_capnp.AddressBook.from_bytes_packed(message)
+
+    print (addresses)
+    return(len(message))
+
+def handle_capnp_client_print_to_file(connection):
+
+    message = connection.recv()
+
+    #addresses = addressbook_capnp.AddressBook.new_message()
+    #addresses = addressbook_capnp.AddressBook.from_bytes(message)
+
+    f = open("schema/addressbook_capnp.bin", "w+b")
+    f.write(message)
+    os.fsync(f)
+    f.close()
+    return(len(message))
+
+def handle_avro_client(connection):
+
+    message = connection.recv()
+
+    message_buf = io.BytesIO(message)
+    reader = avro.datafile.DataFileReader(message_buf, avro.io.DatumReader())
+
+    for thing in reader:
+        print(thing)
+    reader.close()
+    return(len(message))
+    
+def handle_avro_client_print_to_file(connection):
+
+    schema = avro.schema.Parse(open("schema/addressbook.avsc", "rb").read())
+    
+    message = connection.recv()
+
+    message_buf = io.BytesIO(message)
+    reader = avro.datafile.DataFileReader(message_buf, avro.io.DatumReader())
+
+    # Create a data file using DataFileWriter
+
+    dataFile = open("schema/addressbook.avro", "wb")
+
+    writer = DataFileWriter(dataFile, DatumWriter(), schema)
+
+    for thing in reader:
+        writer.append(thing)
+    reader.close()
+    
+    writer.close()
+    return(len(message))
+
+def handle_XML_client(conn):
+
+    message_buf = conn.recv()
+
+    print(message_buf.decode())
+    return(len(message_buf))
+
+def handle_XML_client_print_to_file(conn):
+
+    message_buf = conn.recv()
+
+    f = open("schema/addressbook.xml", "wb")
+
+    f.write(message_buf)
+
+    os.fsync(f)
+    f.close()
+    return(len(message_buf))
+
+def main():
+
+    #0 for just command line printing, 1 for file printing
+    printToFile = sys.argv[1]
+    #0 for protobuf, 1 for cap'n proto, 2 for Apache Avro, and 3 for XML
+    messageType = sys.argv[2]
+    numberOfPeople = sys.argv[3]
+    numberOfMessages = sys.argv[4]
+    numberOfExperiments = sys.argv[5]
+    machinesUsed = sys.argv[6]
+
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    print("Listening")
+
+    times = []
+    message_length_total = []
+    cpu_utilization = []
+    cpu_util_user = []
+    cpu_util_system = []
+    cpu_util_idle = []
+    disk_info1 = []
+    disk_info2 = []
+    disk_info3 = []
+    disk_info4 = []
+    disk_info5 = []
+    disk_info6 = []
+    memory = []
+    net_io_counters1 = []
+    net_io_counters2 = []
+    time_stamp = []
+    message_length = []
+    start_value_disk = psutil.disk_io_counters().write_bytes
+    start_value_bytes_rec = psutil.net_io_counters().bytes_recv
+            
+    while True:
+
+        if int(printToFile) == 0:
+            if int(messageType) == 0:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.cpu_times_percent(None,False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_proto_client(socket))
+            elif int(messageType) == 1:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_capnp_client(socket))
+            elif int(messageType) == 2:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_avro_client(socket))
+            elif int(messageType) == 3:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_XML_client(socket))
+        elif int(printToFile) == 1:
+            if int(messageType) == 0:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_proto_client_print_to_file(socket))
+            elif int(messageType) == 1:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_capnp_client_print_to_file(socket))
+            elif int(messageType) == 2:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_avro_client_print_to_file(socket))
+            elif int(messageType) == 3:
+                start = time.perf_counter()
+                psutil.cpu_percent(None, False)
+                psutil.net_io_counters.cache_clear()
+                psutil.disk_io_counters.cache_clear()
+                message_length.append(handle_XML_client_print_to_file(socket))
+
+        times.append(time.perf_counter() - start)
+        message_length_total.append(message_length)
+        memory.append(psutil.virtual_memory().percent)
+        time_stamp.append(time.perf_counter())
+        cpu_utilization.append(psutil.cpu_percent(None, False))
+        cpu_util_user.append(psutil.cpu_times_percent(None, False).user)
+        cpu_util_system.append(psutil.cpu_times_percent(None, False).system)
+        cpu_util_idle.append(psutil.cpu_times_percent(None, False).idle)
+
+        disk_info1.append(psutil.disk_io_counters().read_count)
+        disk_info2.append(psutil.disk_io_counters().write_count)
+        disk_info3.append(psutil.disk_io_counters().read_bytes)
+        disk_info4.append(psutil.disk_io_counters().write_bytes)
+        disk_info5.append(psutil.disk_io_counters().read_time)
+        disk_info6.append(psutil.disk_io_counters().write_time)
+        net_io_counters1.append(psutil.net_io_counters().bytes_recv)
+
+        with open('results/server_'+str(messageType)+'_'+str(numberOfPeople)+'_'+str(numberOfMessages)+'_'+str(printToFile)+'_'+str(machinesUsed)+'.txt', 'w') as f:
+            f.write('CPU_UTIL:'+str(cpu_utilization)+'\n')
+            f.write('CPU_USER:'+str(cpu_util_user)+'\n')
+            f.write('CPU_SYSTEM:'+str(cpu_util_system)+'\n')
+            f.write('CPU_IDLE:'+str(cpu_util_idle)+'\n')
+            f.write('message_length:'+str(message_length_total)+'\n')
+            f.write('time_stamp:'+str(time_stamp)+'\n')
+            f.write('MEMORY:'+ str(memory)+'\n')
+            f.write('TIMES:'+str(times)+'\n')
+            f.write('read_bytes:'+str(disk_info3)+'\n')
+            f.write('write_bytes:'+str(disk_info4)+'\n')
+            f.write('read_time:'+str(disk_info5)+'\n')
+            f.write('write_time:'+str(disk_info6)+'\n')
+            f.write('bytes_recv:'+str(net_io_counters1)+'\n')
+            f.write('start_value_disk:'+str(start_value_disk)+'\n')
+            f.write('start_value_bytes_rec:'+str(start_value_bytes_rec)+'\n')
+
+        socket.send_string(str(message_length[-1]))
+        #conn.close()
+
+if __name__ == '__main__':
+    main()
